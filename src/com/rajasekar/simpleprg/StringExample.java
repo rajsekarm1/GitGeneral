b8e493b0c8c6eb9358767810689aad17a82f276b
rajsekarm1 <rajsekarm@gmail.com>
2014-04-29 17:03:50 +0530

    Added one file called StringExample

---
+++ src/com/rajasekar/simpleprg/StringExample.java
@@ -0,0 +1,13 @@
+package com.rajasekar.simpleprg;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuilder sb = new StringBuilder();
+		System.out.println(sb.toString());
+
+	}
+
+}
